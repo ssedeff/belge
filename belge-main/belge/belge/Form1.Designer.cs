d12a4ae12f8008df86f586710e45cb4355fd5ffa
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-10-17 16:08:49 +0300

    Add files via upload

---
+++ belge-main/belge/belge/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace belge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNot = new System.Windows.Forms.TextBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Not:";
+            // 
+            // txtNot
+            // 
+            this.txtNot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNot.Location = new System.Drawing.Point(83, 9);
+            this.txtNot.Name = "txtNot";
+            this.txtNot.Size = new System.Drawing.Size(142, 38);
+            this.txtNot.TabIndex = 1;
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKontrol.Location = new System.Drawing.Point(18, 84);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(207, 41);
+            this.btnKontrol.TabIndex = 2;
+            this.btnKontrol.Text = "Kontrol Et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(12, 50);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(86, 31);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 285);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.txtNot);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNot;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ belge-main/belge/belge/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace belge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            float ortalama =
+            ortalama = Convert.ToSingle(txtNot.Text);
+            if (ortalama < 0)
+            {
+                lblSonuc.Text="Negatif sayı girmeyin.";
+            }
+            else if (ortalama < 50)
+            {
+                lblSonuc.Text = "KALDINIZ.";
+            }
+            else if (ortalama < 70)
+            {
+                lblSonuc.Text = "GEÇTİNİZ.";
+            }
+            else if (ortalama < 85)
+            {
+                lblSonuc.Text = "TEŞEKKÜR BELGESİ ALDINIZ.";
+            }
+            else if(ortalama < 100)
+            {
+                lblSonuc.Text = "TAKDİR BELGESİ ALDINIZ.";
+            }
+            else
+            {
+                lblSonuc.Text = "100'den küçük sayı yazın.";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSonuc.Text = " ";
+        }
+    }
+}
